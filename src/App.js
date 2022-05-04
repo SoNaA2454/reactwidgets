c4e01949ae5fe021b7a5286036341341be82bf74
sonali bhandula <sonalibhandula@gmail.com>
2022-05-04 17:23:13 +0530

    reactwidgets routing done

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import Dropdown from "./components/DropDown";
+import Translate from "./components/Translate";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Header from "./components/Header";
+import Route from "./components/Route";
+
+const items = [
+    {
+      title: "What is React?",
+      content: "React is a front end javascript framework",
+    },
+    {
+      title: "Why use React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      title: "How do you use React?",
+      content: "You use React by creating components",
+    },
+  ];
+  
+const options=[
+    {
+        label:'The Color Red',
+        value:'red'
+    },
+    {
+        label:'The Color Green',
+        value:'green'
+    },
+    {
+        label:'The Color Blue',
+        value:'blue'
+    }
+];
+
+const  App= () =>{
+    const[selected,setSelected] = useState(options[0]);
+
+    return (
+        <div>
+            <Header/>
+            <Route path="/">
+                <Accordion items={items}/> 
+            </Route>
+            <Route path="/List">
+                <Search/>
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate/>
+            </Route>
+        </div>
+    )
+}
+export default App;
